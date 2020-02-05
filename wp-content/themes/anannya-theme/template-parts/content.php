b02697161c2a56870bd491b2821770121bf1d225
Hasan Al Banna <rizbanulhasan08@gmail.com>
2020-02-05 17:26:27 +0600

    added true false option

--- wp-content/themes/anannya-theme/template-parts/content.php
+++ wp-content/themes/anannya-theme/template-parts/content.php
@@ -12,8 +12,51 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php anannya_post_thumbnail(); ?>
+			<div class="d_true_false">
+				<div class="d_tf_boxes">
+					<span class="d_tf_button b1"></span>সত্য
+					<span class="d_tf_button b2"></span>মিথ্যা
+					<span class="d_tf_button b3"></span>আংশিক সত্য
+					<span class="d_tf_button b4"></span>আংশিক মিথ্যা
+
+				</div>
+
+				<?php
+            if (has_category('ৎ-সত্য'))
+            { ?>
+                <p class="d_tf_bar1"></p>
+								<?php
+							}
+							 elseif (has_category('ৎ-মিথ্যা'))
+							{
+								?>
+              <p class="d_tf_bar2"></p>
+
+							<?php
+						}
+						 elseif (has_category('ৎ-আংশিক সত্য'))
+						{
+							?>
+
+								<p class="d_tf_bar3"></p>
+								<?php
+							}
+							 elseif (has_category('ৎ-আংশিক মিথ্যা'))
+							{
+								?>
+
+							  <p class="d_tf_bar4"></p>
+								<?php
+							}
+							?>
+
+
+			</div>
 			<div class="d-sm-block d-md-none col-12 d_single_sml">
-				<?php $category = get_the_category(); ?>
+				<?php $category = get_the_category();
+				$category_id = the_category_id(false);
+				?>
+
 					<p class="d_single_cat"> <?php $firstCategory = $category[0]->cat_name; echo $firstCategory;?></p>
 							<p class="d_single_date"><?php echo get_the_date(); ?> , <?php echo get_the_time(); ?></p>
 							<p class="d_last-modified">Last modified: <?php echo get_the_modified_date(); ?> , <?php echo get_the_modified_time(); ?></p>
